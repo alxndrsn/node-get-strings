e3120fa9ab3159f68f7bda20b9bc3e9678b35b59
Alex Anderson <191496+alxndrsn@users.noreply.github.com>
2026-05-03 13:00:24 +0300

    Switch default output to JSON-lines (#24)
    Closes #9

--- test/cli.spec.js
+++ test/cli.spec.js
@@ -38,62 +38,66 @@ describe('get-strings', () => {
   });
 
   describe('output format: null-terminated strings', () => {
-    it('should extract strings from a .js file', () => {
-      // when
-      const actual = execSync(`npx . ./test/examples/simple.js`)
-          .toString()
-          .split('\0');
+    [
+      '--null',
+      '-0',
+    ].forEach(flag => {
+      describe(flag, () => {
+        it('should extract strings from a .js file', () => {
+          // when
+          const actual = execSync(`npx . ${flag} ./test/examples/simple.js`)
+              .toString()
+              .split('\0');
 
-      // expect
-      assert.deepEqual(actual, simpleJsStrings);
+          // expect
+          assert.deepEqual(actual, simpleJsStrings);
+        });
+      });
     });
   });
 
   describe('output format: JSON', () => {
-    it('should extract strings from a .js file', () => {
-      // when
-      const actual = JSON.parse(execSync(`npx . --json ./test/examples/simple.js`).toString());
+    [
+      '--json',
+      '-j',
+    ].forEach(flag => {
+      describe(flag, () => {
+        it('should extract strings from a .js file', () => {
+          // when
+          const actual = JSON.parse(execSync(`npx . ${flag} ./test/examples/simple.js`).toString());
 
-      // expect
-      assert.deepEqual(actual, simpleJsStrings);
+          // expect
+          assert.deepEqual(actual, simpleJsStrings);
+        });
+      });
     });
   });
 
-  it('should provide helpful usage instructions for a .coffee file', () => {
-    try {
-      // when
-      execSync('npx . ./test/examples/non-existent.coffee');
-
-      assert.fail('should have returned non-zero exit code');
-    } catch(err) {
-      // expect
-      assert.equal(err.stdout.toString(), `
-Failed with file: ./test/examples/non-existent.coffee
-
-.coffee not supported; try:
-
-  get-strings <(npx coffee -c "./test/examples/non-existent.coffee")
-`);
-    }
-  });
-
   describe('output format: JSON lines', () => {
-    it('should extract strings from a .js file', () => {
-      // when
-      const raw = execSync(`npx . --jsonl ./test/examples/simple.js`)
-          .toString();
+    [
+      '', // default
+      '--jsonl',
+      '-l',
+    ].forEach(flag => {
+      describe(flag, () => {
+        it('should extract strings from a .js file', () => {
+          // when
+          const raw = execSync(`npx . ${flag} ./test/examples/simple.js`)
+              .toString();
 
-      // then
-      assert.equal(raw.at(-1), '\n');
+          // then
+          assert.equal(raw.at(-1), '\n');
 
-      // when
-      const actual = raw
-          .split('\n')
-          .filter(it => it)
-          .map(line => JSON.parse(line));
+          // when
+          const actual = raw
+              .split('\n')
+              .filter(it => it)
+              .map(line => JSON.parse(line));
 
-      // then
-      assert.deepEqual(actual, simpleJsStrings);
+          // then
+          assert.deepEqual(actual, simpleJsStrings);
+        });
+      });
     });
   });
 
